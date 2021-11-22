58c6570d7f29ef0878427c2cd8ec1167037dcbed
Apesosmarc <73441355+Apesosmarc@users.noreply.github.com>
2021-11-22 10:45:48 -0500

    test configure

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("welcome to home");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000....");
+});
