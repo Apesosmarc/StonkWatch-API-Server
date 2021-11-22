59abcb6b32c53cbcc5a2042871d889d7c5650fa3
Apesosmarc <73441355+Apesosmarc@users.noreply.github.com>
2021-11-22 10:48:09 -0500

    changed PORT to process.env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("welcome to home");
@@ -9,6 +10,6 @@ app.all("*", (req, res) => {
   res.status(404).send("resource not found");
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("server is listening on port 5000....");
 });
