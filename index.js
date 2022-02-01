e191574cf2761007cda908da45d97506f9d2fd80
Marc Apesos <apesos2013@gmail.com>
2022-02-01 11:01:56 -0500

    comment out wakeup script.. killing dynos...

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ app.use(cors());
 //puts data in req.body
 app.use(express.json());
 
-//
+//logger
+app.use(morgan("tiny"));
+
+// adding to avoid CORS error
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -41,7 +44,7 @@ app.use(function (req, res, next) {
 });
 
 //routes
-app.use(morgan("tiny"));
+
 app.use("/api/v1/users", users);
 app.use("/api/v1/watchlists", watchlists);
 app.use("/api/v1/stocks", stocks);
@@ -65,11 +68,11 @@ const start = async () => {
 start();
 
 //function that keeps heroku from auto sleeping free account apps
-const wakeUp = () => {
-  setInterval(() => {
-    http.get("http://floating-lowlands-36240.herokuapp.com");
-    console.log("Wake Up Heroku!");
-  }, 174000);
-};
+// const wakeUp = () => {
+//   setInterval(() => {
+//     http.get("http://floating-lowlands-36240.herokuapp.com");
+//     console.log("Wake Up Heroku!");
+//   }, 174000);
+// };
 
-wakeUp();
+// wakeUp();
